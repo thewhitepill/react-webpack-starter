444fe78f3fe7f78fac9b141bd996d2045e608c21
hacksparr0w <hacksparr0w@protonmail.com>
2024-02-20 22:53:21 +0100

    chore: polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
+    [
+      "@babel/preset-env",
+      {
+        "useBuiltIns": "entry",
+        "corejs": "3.36"
+      }
+    ],
     "@babel/preset-react"
   ],
   plugins: [
